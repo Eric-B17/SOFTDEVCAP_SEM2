a59fd7597e93ad185c8bd417e25539d7cdcb4acd
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2025-02-05 09:06:06 -0500

    Third Commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 navigator.geolocation.getCurrentPosition(position => {
     console.log("Latitude: " + position.coords.latitude);
     console.log("Longitude: " + position.coords.longitude);
-    document.getElementById("locale").innerText = 'latitude: ${position.coords.latitude}, ' +
-        'longitude: ${position.coords.longitude}'
+    document.getElementById("locale").innerText = `latitude: ${position.coords.latitude}, longitude: ${position.coords.longitude}`
 }, error => {
     console.error("Error getting location: ", error);
 });
